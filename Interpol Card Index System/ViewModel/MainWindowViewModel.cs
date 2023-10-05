17e22080fba5b1991da0d7f8a909891befe54401
Rezistencee <86205500+Rezistencee@users.noreply.github.com>
2023-10-05 21:27:14 +0300

    Half of the layout of the main window has been done, a custom font has been added.

---
+++ Interpol Card Index System/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Interpol_Card_Index_System.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private DispatcherTimer _timer;
+
+        public MainWindowViewModel()
+        {
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += Timer_Tick;
+            _timer.Start();
+        }
+
+        private string _currentDateAndTime;
+        public string CurrentDateAndTime
+        {
+            get
+            {
+                return _currentDateAndTime;
+            }
+            set
+            {
+                _currentDateAndTime = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            UpdateCurrentDateTime();
+        }
+
+        private void UpdateCurrentDateTime()
+        {
+            CurrentDateAndTime = DateTime.Now.ToString();
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
