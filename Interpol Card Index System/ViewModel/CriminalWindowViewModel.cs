aa4845c4bc43e117f99d940d6a199487151df5a2
Rezistencee <86205500+Rezistencee@users.noreply.github.com>
2023-10-12 23:06:43 +0300

    Complete crime window layout, create View Model for it.

---
+++ Interpol Card Index System/ViewModel/CriminalWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpol_Card_Index_System.ViewModel
+{
+    public class CriminalWindowViewModel
+    {
+        public CriminalWindowViewModel()
+        {
+
+        }
+    }
+}
